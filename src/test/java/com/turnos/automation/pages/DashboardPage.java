635d83c2004215f43c9a2ad881de8ba46e61ecd3
David Angarita <davidangarita.sistemas@gmail.com>
2026-03-20 11:05:57 -0500

    refactor: remove unused dashboard title element and unused getCurrentUrl method

--- src/test/java/com/turnos/automation/pages/DashboardPage.java
+++ src/test/java/com/turnos/automation/pages/DashboardPage.java
@@ -1,21 +1,12 @@
 package com.turnos.automation.pages;
 
 import net.serenitybdd.core.pages.PageObject;
-import net.serenitybdd.core.pages.WebElementFacade;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.FindBy;
 
 public class DashboardPage extends PageObject {
-
-    @FindBy(css = "h1, h2, [data-testid='dashboard-title']")
-    private WebElementFacade dashboardTitle;
 
     public boolean isLoaded() {
         waitFor(ExpectedConditions.urlContains("/dashboard"));
         return getDriver().getCurrentUrl().contains("/dashboard");
-    }
-
-    public String getCurrentUrl() {
-        return getDriver().getCurrentUrl();
     }
 }
